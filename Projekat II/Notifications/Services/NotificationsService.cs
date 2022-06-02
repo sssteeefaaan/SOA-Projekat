f4660559b0934b08e15f33c7979b8c9b24316bd5
Stefan <stefan.sa.aleksic@gmail.com>
2022-06-02 13:53:29 +0200

    RADIIII

---
+++ Projekat II/Notifications/Services/NotificationsService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+
+namespace Notifications.Services
+{
+    public class NotificationsService:Notifications.NotificationsBase
+    {
+        private readonly ILogger<NotificationsService> _logger;
+        public NotificationsService(ILogger<NotificationsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<Response> Send(Notification payload, ServerCallContext context)
+        {
+            _logger.LogDebug(payload.ToString());
+            Console.WriteLine(payload.ToString());
+            return Task.FromResult(new Response
+            {
+                Status = "Ok"
+            });
+        }
+    }
+}
--- Projekat II/nodejs/server.js
+++ Projekat II/nodejs/server.js
@@ -1,10 +1,29 @@
 require('dotenv');
 
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
 const express = require('express');
 const app = express();
 const env = process?.env;
 
 const PORT = env['PORT'] | 8080;
+
+const PROTO_PATH = __dirname + '/protos/notifications.proto';
+
+// Suggested options for similarity to existing grpc.load behavior
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The protoDescriptor object has the full package hierarchy
+const not_stub = new protoDescriptor.notifications.Notifications(env['GRPC_URL'], grpc.credentials.createInsecure());
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -12,8 +31,24 @@ app.use(express.urlencoded({
 }));
 
 app.post('/test', async(req, res) => {
-    console.log(req.body);
-    return res.send({ "message": "OK!" });
+    try{
+        console.log(req.body);
+        not_stub.send({
+            id:"123",
+            sender: "123",
+            receiver: "123",
+            payload: JSON.stringify(req.body),
+            date: '123'
+        }, (err, response)=>{
+            if(err)console.log(err);
+            else console.log(response);
+        });
+        return res.send({ "message": "OK!" });
+    }
+    catch(e){
+        console.log(e);
+        return res.status(500).send({"error": e});
+    }
 });
 
 app.listen(PORT, ()=>{
